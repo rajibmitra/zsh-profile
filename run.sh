ce9e793ff22be709af281dca7544e531df5489d5
Rajib <rajib.jolite@gmail.com>
2021-09-04 21:35:07 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,5 @@
+sudo apt install curl wget git
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+omz update
